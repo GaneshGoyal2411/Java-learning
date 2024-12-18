07afcb94f5bafcaaa2f2eb04c48aa64ceb668aac
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-18 15:32:15 +0000

    Operartors done

---
+++ 01_basics/Test2_Operators.java
@@ -0,0 +1,41 @@
+/*class Test2_Operators{
+    public static void main(String args[]){
+        System.out.println(3+2);
+        System.out.println(3-2);
+        System.out.println(3*2);
+        System.out.println(3/2);
+    }
+}*/
+
+//-------------- Variables ----------------------
+class Test2_Operators{
+    public static void main(String [] args){
+        // String fName = "Ganesh";
+        // String lName = " Goyal";
+        // String fullName = fName + lName;
+    // //    final int myNum;
+    //    int x = 6;
+    //    int y = 15;
+    //     myNum = 20;
+    //     char myLatter ='G';
+    // int x = 2,y=3,z=6;
+
+    // int x,y,z;?
+    // x=y=z=20;
+
+   /* String studentname = "Ganesh";
+    int studentID = 15, studentAge=25;
+    float studentFee = 75.25f;
+    char studentGrade = 'B';*/
+
+    // Quation - find out the area of the rectngle
+
+    int height = 5, width = 4, area;
+    area = height * width;
+    
+        System.out.println("height is: "+height);
+        System.out.println("Width is: "+width);
+        System.out.println("Area of the Rec is: "+area);
+        // System.out.println("Grade: "+studentGrade);
+    }
+}
