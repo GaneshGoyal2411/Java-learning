31b7ca8664c3cb6ecdc951b799cca04b97800a75
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-20 15:42:38 +0000

    TypeCosting done

---
+++ 01_basics/Test3_typeCosting.java
@@ -0,0 +1,57 @@
+class Test3_typeCosting{
+    public static void main(String args []){
+        /* int myNum = 9;
+        // float numF = myNum;
+        // System.out.println(numF);*/
+
+        int maxScore = 500;
+        int userScore = 423;
+
+        // float percentage = (float) userScore / maxScore * 100.0f;
+       
+        // System.out.println(percentage);
+
+        //The addition assignment operator (+=) adds a value to a variable:
+        int x = 5;
+        x>>=10;
+
+        // greater than operator
+        int five = 5;
+        int three = 3;
+        // System.out.println(five < three);
+
+        // System.out.println(five > three && three > five);
+        // System.out.println(five > three || three > five);
+
+        // System.out.println(!(five < three));
+
+// ---------- String ----------------------------------------
+String greeting = "Hello ganesh for, coming";
+
+// System.out.println(greeting.length());
+// System.out.println(greeting.toUpperCase());
+// System.out.println(greeting.toLowerCase());
+// System.out.println(greeting.indexOf("f"));
+
+// String Concatenation
+// String fName = "Ganesh ";
+// String lName = " Goyal";
+
+// // System.out.println(fName+" "+lName);
+// // System.out.println(fName+lName);
+// System.out.println(fName.concat(lName));
+
+// --------- Adding Numbers and Strings---------
+// String ten = "10";
+// int twnt = 20;
+// String z = ten + twnt;
+// System.out.println(z);
+
+// String greet = "We are going to \"finished\" the by this month";
+// String greet = "It\'s alright";
+
+// String greet = "You\\He";
+String greet = "Hello\t World";
+System.out.println(greet);
+    }
+}
