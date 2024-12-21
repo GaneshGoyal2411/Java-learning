61f3f80b50388a5e58fafd65b5a41d97019238ce
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-21 15:25:01 +0000

    Math done Loops started

--- 01_basics/Test4_Math.java
+++ 01_basics/Test4_Math.java
@@ -1,4 +1,4 @@
-class Test4_Math{
+/*class Test4_Math{
     public static void main(String args []){
 // --------------------- Max & Min ABS ----------------------
         // int x=4,y=5;
@@ -14,12 +14,92 @@ class Test4_Math{
         int myAge = 108;
         int votingAge =25;
         if (myAge >= votingAge){
-            System.out.println("Old enough to vote!");
+        //     System.out.println("Old enough to vote!");
+        // }
+        // else{
+        //     System.out.println("Not enogh old to vote!");
+        // }
+
+        // int time = 19;
+        // if(time < 10){
+        //     System.out.println("Good morning");
+        // }
+        // else if (time < 18) {
+        //     System.out.println("Good day");
+        // }
+        // else{
+        //     System.out.println("Good evening");
+        // }
+
+        // -------------- Ternary ------------------
+
+        // int time = 20;
+        // String result;
+
+        // result = (time < 18) ? "good day" : "good evening";
+
+        // String result = (time < 18) ? "Good day" : "Good evening"; 
+
+        // ---------------- Pra 2----------------------------
+        // int doorCode = 13377;
+        // if(doorCode == 13378){
+        //     System.out.println("Correct code, The door is now open");
+        // }
+        // else{
+        //     System.out.println("Wrong code, The door remains closed");
+        // }
+
+        //------------------ Pra -3 ---------------------
+        int num = 100;
+        // if (num < 0){
+        //     System.out.println(num+" is negative");
+        // } else if(num > 0){
+        //     System.out.println(num +" is positive");
+        // }else{
+        //     System.out.println("Number is "+num);
+        // }
+
+        // -------------- Pra - 4 -------------------------
+        // if (num % 2 == 0){
+        //     System.out.println(num +" is even");
+        // }else {
+        //     System.out.println(num+ " is odd");
+        // }
+
+        // -------------------- Switch case ----------------------
+        int days = 10;
+        switch (days) {
+            case 1:
+                System.out.println("it's monday");
+                break;
+        
+            case 2:
+                System.out.println("it's Tuesday");
+                break;
+        
+            case 3:
+                System.out.println("It's Wedneday");
+                break;
+            case 4:
+            System.out.println("It's Thursday");
+                break;
+            case 5:
+            System.out.println("It's Friday");
+                break;
+            case 6:
+            System.out.println("It's Saturday");
+                break;
+        
+            case 7:
+            System.out.println("It's Sunday");
+                break;
+        
+            default:
+            System.out.println("It's not come in week days");
+                break;
         }
-        else{
-            System.out.println("Not enogh old to vote!");
-        }
+        
 
         // System.out.println(myAge <= votingAge);
     }
-}
+}}*/
