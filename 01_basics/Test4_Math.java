a3af3bad015c4c9d670d7e0dd41905321cf8631b
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-21 11:10:38 +0000

    Math

---
+++ 01_basics/Test4_Math.java
@@ -0,0 +1,25 @@
+class Test4_Math{
+    public static void main(String args []){
+// --------------------- Max & Min ABS ----------------------
+        // int x=4,y=5;
+        int x = 0;
+        x--;
+        // System.out.println(Math.max(x,y));
+        // System.out.println(Math.min(x,y));
+        // System.out.println(Math.abs(x));
+
+        // ------------- Boolean --------------------
+        // boolean isJavaFun = true;
+        // boolean isFishTasty = false;
+        int myAge = 108;
+        int votingAge =25;
+        if (myAge >= votingAge){
+            System.out.println("Old enough to vote!");
+        }
+        else{
+            System.out.println("Not enogh old to vote!");
+        }
+
+        // System.out.println(myAge <= votingAge);
+    }
+}
