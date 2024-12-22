245d10e5721004015ed053108be11a1d2700655d
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-22 12:11:43 +0000

    While and Do While done

---
+++ 01_basics/Test5_loops.java
@@ -0,0 +1,47 @@
+class Test5_loops{
+    public static void main(String args []){
+
+ // ----------------- While --------------------
+        // int i = 4;
+        // while(i<5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+// ----------------- Do While -------------------
+            // do {
+            //     System.out.println(i);
+            //     i++;
+            // }
+            // while(i!=5);  
+
+// ----------- Pra - of While ---------------------
+            // int coundown = 3;
+            // while(coundown>0){
+            //     System.out.println(coundown);
+            //     coundown--;
+            // }
+            // System.out.println("Happy New Year!");
+
+// ----------- While + if + else ----------------------
+            // int dise = 1;
+            // while (dise <= 6){
+            //     if(dise < 6){
+            //         System.out.println("No Yatzy!");
+            //     }
+            //     else{
+            //         System.out.println("Yatzy");
+            //     }
+            //     dise++;
+            // }
+
+//--------------------- By Self -----------------------   
+                int dise = 1;
+                while(dise < 6){
+                    System.out.println("No Yatzy");
+                    dise ++;
+                }
+                
+                System.out.println("Yatzy!");
+}
+}
