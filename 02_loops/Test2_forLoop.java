b8452c2c6d2abfa7b421e0aec54d5a3095765614
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-23 15:28:49 +0000

    For loop done

---
+++ 02_loops/Test2_forLoop.java
@@ -0,0 +1,79 @@
+class Test5_loops{
+    public static void main(String args []){
+        // for (int i = 0; i <= 4; i++){
+        //     System.out.println(i);
+        // }
+
+// 2)--------------------------- 0 to 10 -------------------------------
+            
+        // for (int i = 0; i<=10; i = i+2){
+        //     System.out.println(i);
+        // }
+    
+//------------------------- Self ---------------------------------------
+        // for(int i=0;i<=10;i++){
+            //     if(i%2==0){
+            //         System.out.println(i);
+            //     }
+            // }
+
+//------------------- 3 (Nested loop)-------------------------------------
+                // for(int i = 1; i<=2; i++){
+                //     System.out.println("Outer:"+i);
+                //     for(int j = 1; j<=3; j++){
+                //         System.out.println("Inner:"+j);
+                //     }
+                // }
+
+//----------------------------- Self-----------------------------                
+// for (int i = 1; i <= 2; i++) {
+//     for (int j = 1; j <= 3; j++) {
+//       System.out.print(i + "," + j + " ");
+//     }
+//   }
+
+// ---------------------- For - Each loop ------------------------
+// for (type variableName : arrayName) {
+//     // code block to be executed
+//   }
+
+String [] cars = {"Farrari","Toyoto","BMW"};
+// for(String i : cars){
+//     System.out.println(i);
+// }
+
+//--------------------------- Self --------------------------
+    // int [] nums = {1,2,3,4,5};
+    // for(int i : nums){
+    //     // System.out.println(nums[2]);
+    // }
+    // System.out.println(nums[4]);
+
+// -------------------- Real Life Example -------------------- 10 to 100    
+        // for (int i = 0; i<=100; i += 10){
+        // System.out.println(i);
+        // }
+
+//-------------------------- self ----------------------------------
+            // for (int i = 0; i<100;){
+            //     i = i +10;
+            //     System.out.println(i);
+            // }
+
+// ------------------- One More -------------------------------------
+                // for (int i = 0; i<=10; i += 2){
+                //     System.out.println(i);
+                // }
+
+                // for(int i = 0; i<=10;){
+                //     i += 2;
+                //     System.out.println(i);
+                // }
+
+// -------------------- 2 Ka Table --------------------
+            int number = 2;                
+                    for (int i = 1; i<=10; i++){
+                        System.out.println(number+"x"+i+"="+ number * i);
+                    }
+        }
+}
