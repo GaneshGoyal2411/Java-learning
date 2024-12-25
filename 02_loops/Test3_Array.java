d24ff8f4dc5dd7ab09ac230e639ca17f56e151e8
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-25 15:39:43 +0000

    Arrays done

--- 02_loops/Test3_Array.java
+++ 02_loops/Test3_Array.java
@@ -19,7 +19,7 @@ class Test3_Array{
         // }     
         
 // ----------------- Real-Life Example ----------------- Find out the Avr age
-        int [] ages = {20,22,26,21,28,27};
+        // int [] ages = {20,22,26,21,28,27};
         // int avr, sum = 0;
         // int length = ages.length;
         // System.out.println("Length="+length);
@@ -32,13 +32,41 @@ class Test3_Array{
         // System.out.println(sum);
         // System.out.println(avr);
 
-       int lowestAge = ages[0];
-        for (int age : ages){
-            if(age < lowestAge){
-                lowestAge = age;
+    //    int lowestAge = ages[0];
+    //     for (int age : ages){
+    //         if(age < lowestAge){
+    //             lowestAge = age;
+    //         }
+    //     }
+    //     System.out.println(lowestAge);
+
+// ------------------- Multidimensional Arrays -------------    
+            // int [][] myNumbers = {{1,2,3},{4,3,6}};
+        //     if(myNumbers[1][1] == 5){  // SELF ------------
+        //     myNumbers[1][1] = 9;
+        //     System.out.println(myNumbers[1][1]);
+        // }
+        // else{
+        //     myNumbers[1][1] = 100;
+        //     System.out.println(myNumbers[1][1]);
+        // }
+
+// ----------------- Loop Through a Multi-Dimensional Array -----------------------
+            // int [][] myNumbers = {{1,2,3,4},{5,6,7}};
+            // for(int i = 0; i<myNumbers.length; i++){
+            //     for(int j = 0; j<myNumbers[i].length; j++){
+            //         System.out.println(myNumbers[i][j]);
+                
+            //     }
+            // }
+            // System.out.println(myNumbers[0].length+myNumbers[1].length);
+
+// ----------------------- Or you could just use a for-each loop, which is considered easier to read and write:------------------            
+            int [][] myNumbers = {{1,2,3,4},{5,6,7}};
+            for(int [] row : myNumbers){
+                for(int i : row){
+                    System.out.println(i);
+                }
             }
-        }
-        System.out.println(lowestAge);
-            
-    }
+}
 }
