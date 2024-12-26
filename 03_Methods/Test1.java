b85db750e2cf122e0cdd6f3df489cc8e8fbaef4a
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-26 09:40:01 +0000

    Methods with VOID so far

--- 03_Methods/Test1.java
+++ 03_Methods/Test1.java
@@ -12,10 +12,43 @@ class Test1{
         // }
 
 // ----------------------- Parameters and Arguments -------------------------------
-        static void myMethod(String fname){
-            System.out.println(fname+"Goya");
+        // static void myMethod(String fname){
+        //     System.out.println("Full Name:"+fname+" Goyal");
+        // }
+        //     public static void main(String args[]){
+        //     myMethod("Ganesh");
+        //     myMethod("Badal");
+        //     }
+
+// -------------------- Multiple Parameters----------------------
+
+        // static void myMethod(String fname, int age){
+        //         System.out.println("Name is "+fname+" with Age:"+age);
+        // }
+
+        // public static void main(String args[]){
+        //         myMethod("Ganesh",100);
+        // }
+
+// --------------------- A Method with If...Else ----------------------
+
+        // static void checkAge(int age){
+        //         if (age < 18){
+        //                 System.out.println("with "+age+" years age you are not enough old");
+        //         }
+        //         else{
+        //                 System.out.println("with "+ age+ " years age you are enough old");
+        //         }
+        // }
+
+        // public static void main(String args []){
+        //         checkAge(18);
+        // }
+
+
+// 02) ----------------- Return Values (Without VOID) ----------------------
+        static int myMethod(int age){
+                
         }
-            public static void main(String args[]){
-            myMethod("Ganesh");
-            }
+
     }
