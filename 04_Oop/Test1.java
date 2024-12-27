32895ac86dff41f5a4cf10b3b7aebaa55462a0ab
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-27 09:47:07 +0000

    Oop Started

--- 04_Oop/Test1.java
+++ 04_Oop/Test1.java
@@ -2,8 +2,10 @@ public class Test1{
     int x=5;
 
     public static void main(String args []){
-        Test1 myObj = new Test1();
-        System.out.println(myObj.x);
+        Test1 myObj1 = new Test1();
+        Test1 myObj2 = new Test1();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
     }
 }
 
