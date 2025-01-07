323d49450852620f9fe90f2948dd86a0679b943f
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-07 15:26:00 +0000

    Encapsulation done

--- Encapsulation_GetSet/Test1.java
+++ Encapsulation_GetSet/Test1.java
@@ -1,12 +1,30 @@
-public class Test1 {
-    private String name;    
+class Person{
+    private String name;  
+    private String mobileNumber;  
+    // getter
+    public String getName(){
+        return name;
+    }
+    public String getMobileNumber(){
+        return mobileNumber;
+    }
+    // Setter
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setMobileNumber(String newNumber){
+        this.mobileNumber = newNumber;
+    }
+}
 
-// getter
-public String getName(){
-    return name;
-}
-// Setter
-public void setName(String newName){
-    this.name = newName;
-}
+public class Test1 {
+    public static void main(String[] args) {
+        Person obj = new Person();
+        // obj.name = "ganesh";
+        // System.out.println(obj.name);
+        obj.setName("Ganesh");
+        obj.setMobileNumber("9575556665");
+        System.out.println(obj.getName());
+        System.out.println(obj.getMobileNumber());
+    }
 }
