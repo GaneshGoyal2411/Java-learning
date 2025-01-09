ca22788d2416b8c9a8d5da209806e7df4d34dd86
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-09 10:16:36 +0000

    Package completed and inheritance started

---
+++ 08_packages/01_First.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+// import java.util.*;
+public class 01_First{
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String name;
+        System.out.println("Enter your name");
+        name = myObj.nextLine();
+
+        System.out.println(name);
+    }
+}
---
+++ 08_packages/MyPackageClass.java
@@ -0,0 +1,7 @@
+package mypack; // HOW TO COMPILE THE PACKAGE = C:\Users\Your Name>javac -d . MyPackageClass.java
+
+public class MyPackageClass {
+    public static void main(String args []){
+        System.out.println("This is my package");
+    }
+}
