6228b1e24496313482c02692eadefaa407ed9f95
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-16 15:21:46 +0000

    Switch started

---
+++ 14_enum/One.java
@@ -0,0 +1,28 @@
+/*enum Level{
+    Low,
+    MEDIUM,
+    HIGH
+}
+
+public class One {
+    public static void main(String[] args) {
+        Level myObj = Level.Low;
+        System.out.println(myObj);
+    }
+}*/
+
+// ------------- Sec one-------------------------
+/*public class One{
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public static void main(String[] args) {
+        Level mLevel = Level.HIGH;
+        System.out.println(mLevel);
+    }
+}*/
+
+// ---------- With switch case ---------------------
+
