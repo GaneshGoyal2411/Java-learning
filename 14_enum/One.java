140840a383e7928bc6d69e3ccd28c39670d4f3f0
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-17 11:02:08 +0000

    enum special class done

--- 14_enum/One.java
+++ 14_enum/One.java
@@ -25,4 +25,49 @@ public class One {
 }*/
 
 // ---------- With switch case ---------------------
+/*
+public class One{
+    enum Demo{
+        LOW,
+        MEDIUM,
+        HIGH,
+        reset
+    }
+
+    public static void main(String[] args) {
+        Demo myObj = Demo.reset;
+        System.out.println(myObj);
+
+        switch (myObj) {
+            case LOW:
+                System.out.println("It's low level called");
+                break;
+            case MEDIUM:
+                System.out.println("It's medium level called");
+                break;
+            case HIGH:
+                System.out.println("It's High level called");
+                break;
+        
+            default:
+            System.out.println("It's unknown level called, please reset it");
+                break;
+        }
+    }
+} */
+
+//------------------- With For each loop-------------------------------------
+public class One{
+    enum Level{
+        HIGH,
+        MEDIUM,
+        LOW
+    }
+
+    public static void main(String args[]){
+        for(Level myLevel: Level.values()){
+            System.out.println(myLevel);
+        }
+    }
+}
 
