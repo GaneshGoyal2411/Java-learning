cba0fc4b7eabd6239100bc249dd808a903117966
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-21 09:49:09 +0000

    arrayList started

---
+++ 16_arrayList/One.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+class One{
+    public static void main(String args[]){
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("volvo");
+        cars.add("toofan");
+        cars.add("baleno");
+        cars.add("maruti");
+/*
+        cars.add(0,"thor");
+        System.out.println(cars);
+        System.out.println(cars.get(2)); // Get -------------------------------
+
+        cars.set(0,"fortuner"); // Set / exchange------------------------------
+        System.out.println(cars);
+
+        cars.remove(3); // Remove ----------------------------------------------
+        System.out.println(cars);
+        // System.out.println(cars.remove(0)); // Additional--------------------------
+        // System.out.println(cars);
+
+        // cars.clear(); // Clear --------------------------------------------------------
+       // Size ----------------------------------------------------------
+        System.out.println(cars.size()); */
+
+        // for Loop---------------------------------------------------------------------------------------------------------------------
+        for(int i=0; i<cars.size(); i++){
+            System.out.println(cars.get(i));
+        }
+    }
+}
