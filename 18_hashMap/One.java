51b0f1c0ba4c16a89091bdb9d4022057a84f9ca2
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-22 06:00:02 +0000

    HashMap started

---
+++ 18_hashMap/One.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+class One{
+    public static void main(String args[]){
+        HashMap<String , String> capitalCities = new HashMap<>();
+
+        capitalCities.put("MP", "Indore");
+        capitalCities.put("MP", "Bhopal");
+        capitalCities.put("UP", "Agra");
+        capitalCities.put("CG", "Raipur");
+        System.out.println(capitalCities);
+
+        // for(int i=0; i<capitalCities.size(); i++){
+        //     System.out.println(capitalCities.get(i));
+        // }
+        for(String city : )
+    }
+}
