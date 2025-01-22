216768ea9c79138c080bbf3751b654ede5a07445
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-22 08:41:49 +0000

    HashMap basic done

--- 18_hashMap/One.java
+++ 18_hashMap/One.java
@@ -4,13 +4,17 @@ class One{
         HashMap<String , String> capitalCities = new HashMap<>();
 
         capitalCities.put("MP", "Indore");
-        capitalCities.put("MP", "Bhopal");
+        capitalCities.put("Indore", "Bhopal");
         capitalCities.put("UP", "Agra");
         capitalCities.put("CG", "Raipur");
-        capitalCities.remove("MP");
+        // capitalCities.remove("MP");
         // System.out.println(capitalCities.remove("MP"));
         System.out.println(capitalCities);
-
+        System.out.println(capitalCities.get("Indore")); // Get
+        capitalCities.remove("Indore"); // remove
+        // capitalCities.clear(); // remove all
+        System.out.println(capitalCities);
+        System.out.println(capitalCities.size());
         // for(int i=0; i<capitalCities.size(); i++){
         //     System.out.println(capitalCities.get("MP"));
         // }
